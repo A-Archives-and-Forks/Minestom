7207068685c192e3964562b1b780a0ada4a99a00
kermandev <46640204+kermandev@users.noreply.github.com>
2025-07-14 19:30:25 -0500

    Add translation to Testing

--- testing/src/main/java/module-info.java
+++ testing/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module net.minestom.testing {
     requires static org.jspecify;
     requires static org.jetbrains.annotations; // TODO Remove when JSpecify is mature
     requires net.minestom.server;
+    requires net.kyori.adventure;
     requires net.kyori.adventure.nbt;
     requires org.junit.jupiter.api;
 
